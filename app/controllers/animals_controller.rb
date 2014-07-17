bfd80a0fb6969292a994e2efa06571b9bc8697cc
Trevor Jones <trevordavidjones@gmail.com>
2014-07-16 19:16:14 -0500

    adding a parrot

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -5,4 +5,8 @@ class AnimalsController < ApplicationController
   def lion
     @lion = Lion.new
   end
+
+  def parrot
+    @parrot = Parrot.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/lion' => 'animals#lion', :as => :lion
 
+
+  get '/parrot' => 'animals#parrot', as: :parrot
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
